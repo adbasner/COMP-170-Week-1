9f2a86ae526089e873b8e3ccd18beea371eeca92
adbasner <andrewbasner@gmail.com>
2024-01-17 10:28:15 -0600

    first commit

---
+++ weekOne/src/WeekOne.java
@@ -0,0 +1,10 @@
+public class WeekOne {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        testString("Andrew", 41);
+    }
+    
+    public static void testString(String fname, int age){
+        System.out.println("Hello, my name is " + fname + " and I am " + age + " years old!");
+    }
+}
